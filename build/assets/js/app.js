22d836ffbad4c33db7d39dd56276f0d9ccec8ac5
Max <vip.pokryshko@mail.ru>
2022-03-11 21:31:43 +0300

    GG

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,15 @@
-new Swiper('.swiper');
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
